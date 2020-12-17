c9013f8cc5eb5746d139ac642d5ec6e4ad1f7657
konexio <damir.sagadbekov@live.fr>
2020-12-17 17:44:25 +0100

    ouderiba

--- jour-7/exo.js
+++ jour-7/exo.js
@@ -25,3 +25,43 @@ function countEach (string){
     }
 }
 console.log(countEach("xxxoooxo"));
+
+// 03 - Palindrome
+
+function checkPal (string){
+    if ( string.split("").reverse().join("") === string ){
+        console.log("Palindrome !!!")
+    } else {
+        console.log("Nope ...")
+    }
+}
+
+checkPal("kayak")
+
+// 04 - Swap
+
+function swap (string){
+    var hello = string;
+    var newWord = [];
+    for (var i = 0; i < hello.length; i++) {
+        if (hello[i] == hello[i].toLowerCase()) {
+            newWord[i] = hello[i].toUpperCase();
+        }
+        else {
+          newWord[i] = hello[i].toLowerCase();
+        }
+      } return newWord.join("")
+}    
+console.log(swap("Hello World"));
+
+// Bonus
+
+function makeItSpongeBob (string){
+    var hello = string;
+    var newWord = [];
+    for (var i = 0; i < hello.length; i = i + 2){
+        newWord = hello.toUpperCase();
+    }
+    return newWord;
+}
+console.log(makeItSpongeBob("Javascript is easy"));
